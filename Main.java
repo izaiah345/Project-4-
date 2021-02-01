4f61a73310b5b1fb9887b7ef906a4235e030001d
izaiah365 <izaiah.rodriguez@patelhs.org>
2021-02-01 18:52:25 +0000

    I have to add a new method

--- Main.java
+++ Main.java
@@ -17,25 +17,11 @@ class Main {
   System.out.println(greeting(name));
   System.out.println("You enounterd true love in year was I right?" + age);
   
-  System.out.println("lover 1 + 1 lover equals? " + Sum(1,1));
+  
 
  }
 
 
   //This concpecpt makes sure that the person gets their couple 
- static int getAge(int birthMonth, int currentYear)
- {
- int age = birthMonth - currentYear;
- return age;
- }
-  static String greeting(String name)
- {
-  String hello = "Hello "+ name;
-  return hello; 
- }
- // new method    
-static int Sum(int num1, int num2)
- {
-return num1 + num2;
- }
+ 
 }
