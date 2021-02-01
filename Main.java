a68b489606e94449006fd017387e227d48a6ee5a
izaiah365 <izaiah.rodriguez@patelhs.org>
2021-02-01 18:02:46 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
  public static void main(String[] args) 
  {
   Scanner scan = new Scanner(System.in);
-
+// this takes in the costmers information and pass it through the code.
   System.out.println("What is your name");
   String name = scan.next();
   System.out.println("What year is it");
@@ -13,7 +13,7 @@ class Main {
   int birthYear = scan.nextInt();
  
   int age = getAge(birthYear, currentYear);
-
+// this takes a qoute and puts into an output 
   System.out.println(greeting(name));
   System.out.println("You enounterd true love in year was I right?" + age);
   
@@ -22,7 +22,7 @@ class Main {
  }
 
 
-  //This concpecpt makes sure that the 
+  //This concpecpt makes sure that the person gets their couple 
  static int getAge(int birthMonth, int currentYear)
  {
  int age = birthMonth - currentYear;
